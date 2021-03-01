651f68e845d8a6c0877cd3f045a4775113cdbd13
iannyip <ianyip@nullspace.com.sg>
2021-03-01 12:42:57 +0800

    dogs and quotes route base complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import pg from "pg";
 import cookieParser from 'cookie-parser';
 import methodOverride from 'method-override';
+import moment from 'moment';
 
 // Set up
 const {Pool} = pg;
@@ -17,11 +18,62 @@ const pool = new Pool(pgConnectionConfig);
 const app = express();
 app.set('view engine', 'ejs');
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 // Create routes
 app.get('/', (request, response) => {
   response.render('root');
+})
+
+app.get('/quotes', (request, response) => {
+  pool
+    .query(`SELECT * FROM quotes`)
+    .then((result) => {
+      const allQuotes = result.rows;
+      response.render('quotes', {allQuotes});
+    })
+    .catch((error) => console.log("error: ", error.stack))
+})
+
+app.get('/quote/new', (request, response) => {
+  const {userId} = request.cookies;
+  response.render('quote-new', {userId});
+})
+
+app.post('/quote/new', (request, response) => {
+  const newQuoteData = request.body;
+  console.log(newQuoteData);
+  pool
+    .query(`INSERT INTO quotes (quote, quoter_id) VALUES ('${newQuoteData.quote}', ${Number(newQuoteData.quoter_id)})`)
+    .then((result) => response.redirect('/quotes'))
+    .catch((error) => console.log(error));
+})
+
+app.get('/quote/:id/edit', (request, response) => {
+  const {id} = request.params;
+  pool
+    .query(`SELECT * FROM quotes WHERE id=${id}`)
+    .then((result) => {
+      response.render('quote-edit', result.rows[0]);
+    })
+    .catch((error) => console.log(error));
+})
+
+app.put('/quote/:id/edit', (request, response) => {
+  const {id} = request.params;
+  pool
+    .query(`UPDATE quotes SET quote='${request.body.quote}' WHERE id=${id}`)
+    .then((result) => response.redirect('/dogs/you'))
+    .catch((error) => console.log(error));
+})
+
+app.delete('/quote/:id/delete', (request, response) => {
+  const {id} = request.params;
+  pool
+    .query(`DELETE FROM quotes WHERE id=${id}`)
+    .then((result) => response.redirect('/dogs/you'))
+    .catch((error) => console.log(error));
 })
 
 app.get('/dogs', (request, response) => {
@@ -34,20 +86,34 @@ app.get('/dogs', (request, response) => {
 
 app.get('/dogs/profile/:id', (request, response) => {
   const {id} = request.params;
+  const userData = {};
   pool
     .query(`SELECT * FROM dogs WHERE id=${id}`)
     .then((result) => {
-      response.render('dog-single', result.rows[0]);
+      userData.info = result.rows[0];
+      return pool.query(`SELECT * FROM quotes WHERE quoter_id=${id}`)
     })
-    .catch((error) => console.log("error banana: ", error.stack));
+    .then((result) => {
+      userData.quotes = result.rows;
+      console.log(userData);
+      response.render('dog-single', userData);
+    })
+    .catch((error) => console.log("error: ", error))
 })
 
 app.get('/dogs/you', (request, response) => {
   const {userId} = request.cookies;
+  const userData = {};
   pool
     .query(`SELECT * FROM dogs WHERE id=${userId}`)
     .then((result) => {
-      response.render('dogs-you', result.rows[0]);
+      userData.info = result.rows[0];
+      return pool.query(`SELECT * FROM quotes WHERE quoter_id=${userId}`)
+    })
+    .then((result) => {
+      userData.quotes = result.rows;
+      console.log(userData);
+      response.render('dogs-you', userData);
     })
     .catch((error) => console.log("error: ", error))
 })
@@ -57,17 +123,25 @@ app.get('/dogs/you/edit', (request, response) => {
   pool
     .query(`SELECT * FROM dogs WHERE id=${userId}`)
     .then((result) => {
-      response.render('dogs-you-edit', result.rows[0]);
+      const dogObj = result.rows[0];
+      dogObj.date = moment(dogObj.dob).format("YYYY-MM-DD");
+      response.render('dogs-you-edit', dogObj);
     })
     .catch((error) => console.log("error: ", error))
 })
 
 app.put('/dogs/you/edit', (request, response) => {
-  const updatedData = request.body;
-  console.log(updatedData);
-  console.log('banana');
-  
+  const formData = request.body;
+  const insertData = [formData.name, formData.dob, formData.about, formData.status];
+  const {userId} = request.cookies;
+  pool 
+    .query(`UPDATE dogs SET (name, dob, about, status) = ($1, $2, $3, $4) WHERE id=${userId}`, insertData)
+    .then((result) => {
+      response.redirect('/dogs/you');
+    })
+    .catch((error) => console.log("error: ", error))
 })
+
 
 
 // Start server
